7d852b1e0f559a57e6907174fdc3b7dbca1f2364
Hendrik Human <61615033+jhhuman@users.noreply.github.com>
2020-11-17 16:19:20 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,7 @@ export default function Home() {
         </p>
 
         <div className={styles.grid}>
-          <Image src="/iphone-1.jpg" alt="my image" quality="100"/>
+          <Image src="/iphone-1.jpg" alt="my image"/>
 
           <a href="https://nextjs.org/learn" className={styles.card}>
             <h3>Learn &rarr;</h3>
